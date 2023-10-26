8e83c6a2cd39c2e6ed2a55fcc3c2947abb774f8e
Kevin Chavez <kevin@dexterity.ai>
2023-10-26 08:43:27 -0700

    poetry: first code poem

---
+++ poem-number-one.py
@@ -0,0 +1,45 @@
+# In the garden of code,
+# Where variables bloom like flowers,
+sunrise = "start of a new day"
+coffee = {"mug", "instant coffee", "half and half", "stevia"}
+EB = "EB"
+
+# We initialize our hearts with hope,
+love = None
+
+# As we walk through the branches of logic,
+if sunrise and coffee and EB:
+    # Embrace the light of opportunities,
+    love = True
+
+# We rev up our creative engines,
+our_creation = []
+def create():
+    our_creation.append("☆☆☆ magic ☆☆☆")
+
+
+# Noting that the creation is always in progress,
+def is_done(creation):
+    return False
+
+
+# We find our voices,
+def tell_story():
+    print(our_creation)
+
+
+# And as the day unfolds,
+while love:
+    # We dance in the fields of creativity,
+    create()
+
+    # With every line, a story is told,
+    tell_story()
+
+    # Maybe some day... the creation will be done.
+    # But for now we continue. Step by beautiful step.
+    if is_done(our_creation):
+        break
+
+# Let this poem fill your screen with magic
+
